f37f3818b6cde6b14e9d0c82d4991eadf845c8dd
EddyPBR <edvaldojunior1310@gmail.com>
2020-08-03 16:21:54 -0300

    Adicionando Landing page e preparando as rotas para outras paginas

---
+++ web/src/pages/TeacherForm/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import "./styles.css";
+
+const TeacherForm = () => {
+  return <h1>Teacher form</h1>;
+};
+
+export default TeacherForm;
---
+++ web/src/pages/TeacherList/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import "./styles.css";
+
+const TeacherList = () => {
+  return <h1>Teacher List</h1>;
+};
+
+export default TeacherList;
