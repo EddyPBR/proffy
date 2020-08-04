216361383756df5b02d60bd250b522f9a0b8b4a7
EddyPBR <edvaldojunior1310@gmail.com>
2020-08-04 15:23:08 -0300

    Adicionando a funcionalidade de cadastrar e separando responsabilidades do controller

--- server/src/database/migrations/02_create_class_schedule.ts
+++ server/src/database/migrations/02_create_class_schedule.ts
@@ -1,13 +1,13 @@
 import knex from "knex";
 
 export async function up(knex: knex) {
-  return knex.schema.createTable("classes_schedule", (table) => {
+  return knex.schema.createTable("class_schedule", (table) => {
     table.increments("id").primary();
     table.integer("week_day").notNullable();
     table.integer("from").notNullable();
     table.integer("to").notNullable();
     table
-      .integer("classes_id")
+      .integer("class_id")
       .notNullable()
       .references("id")
       .inTable("classes")
